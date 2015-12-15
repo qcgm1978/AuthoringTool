f1b1e1db232029c0ac812fa5da17b815fadfd9f3
liuqiang <liuqiang@collegepre.com>
2015-12-15 10:58:49 +0800

    Signed-off-by: liuqiang <liuqiang@collegepre.com>

--- modules/editorbar/EditorBar.jsx
+++ modules/editorbar/EditorBar.jsx
@@ -6,39 +6,70 @@ var $ = require("jquery");
 
 var EditBar = React.createClass({
     initialOptions : {
-        panelId: "#j-edit-panel",
-        barId : "#j-edit-bar",
-        editTextId: "#j-edit-text",
-        gridClass : ".j-gridster",
-        panelText: ".edit-panel-text",
-        gridBlockClass: ".j-grid-block",
-        editorClass: ".wysiwyg-editor",
+        _dot : ".",
+        _id: "#",
+        panelId: "j-edit-panel",
+        barId : "j-edit-bar",
+        txtPanle: "j-panel-text",
+        imgPanel: "j-panel-img",
+        panelText: "edit-panel-text",
+        gridBlockClass: "j-grid-block",
         hideClass: "hide",
         dataPanelIndex: "data-panel-index",
         dataBarIndex: "data-bar-index",
-        placeHolder : "Type your text here..."
+        placeHolder : "Type your text here...",
+        myGridContainer: "j-grid-container",
+        modifiersOpts : ['bold', 'italic', 'underline', 'h1', 'h2', 'ol', 'ul']
+
 
     },
-    panelOpenHandler: function () {
-        var _dateBarIndex = $(this.initialOptions.barId).attr(this.initialOptions.dataBarIndex),
-            $li = $(this.initialOptions.gridBlockClass).eq(_dateBarIndex);
+    showPanelFn : function (event) {
+        var _dateBarIndex = $(this.initialOptions._id + this.initialOptions.barId).attr(this.initialOptions.dataBarIndex),
+            $li = $(this.initialOptions._dot + this.initialOptions.gridBlockClass).eq(_dateBarIndex);
+        var $myDiv = $li.find(this.initialOptions._dot + this.initialOptions.myGridContainer);
+        if ($myDiv.length< 1) {
+            var $div = $("<div>", {
+                class :this.initialOptions.myGridContainer
+            });
+            $li.append($div);
+        }
         var $offset = $li.offset(),
             _top = $offset.top,
             _left = $offset.left +  100;
-        var $textArea = $('<textarea id="j-edit-text" name="editor" placeholder="' + this.initialOptions.placeHolder + '"></textarea>');
+        var $panel = $(this.initialOptions._id + this.initialOptions.panelId);
+        $panel.find(this.initialOptions._dot + this.initialOptions.txtPanle).addClass(this.initialOptions.hideClass);
+        $panel.find(this.initialOptions._dot + this.initialOptions.imgPanel).addClass(this.initialOptions.hideClass);
+        $panel.hide("fast");
+        $panel.css({"top":_top -23,"left":_left}).attr(this.initialOptions.dataPanelIndex, _dateBarIndex).fadeIn();
 
-        $(this.initialOptions.panelId).css({"top":_top -23,"left":_left}).attr(this.initialOptions.dataPanelIndex, _dateBarIndex)
-            .removeClass(this.initialOptions.hideClass)
-            .find(this.initialOptions.panelText).empty().append($textArea);
-
-        $(this.initialOptions.editTextId).myTextEditor();
-
-
-        var $exitTemp = $li.find(this.initialOptions.editorClass),
-            $panelEditor =  $(this.initialOptions.panelId).find(this.initialOptions.editorClass);
-        if ($exitTemp.length) {
-            $panelEditor.empty().append($exitTemp.html());
+        return {
+            _panel : $panel,
+            _li : $li
         }
+
+    },
+    txtEditOpenFn: function ($panel, $li) {
+        var arr= this.showPanelFn();
+
+        arr._panel.find(this.initialOptions._dot + this.initialOptions.txtPanle).removeClass(this.initialOptions.hideClass);
+
+        $(this.initialOptions._dot + this.initialOptions.panelText).empty().notebook({
+            autoFocus: true,
+            placeholder: this.initialOptions.placeHolder,
+            modifiers: this.initialOptions.modifiersOpts
+        });
+
+        var $textContainer = arr._li.find(this.initialOptions._dot + this.initialOptions.myGridContainer),
+            $exitTextChild = $textContainer.children(),
+            _textHtml =$textContainer.html(),
+            $panelEditor =  $(this.initialOptions._dot + this.initialOptions.panelText);
+        if ($exitTextChild.length >0 ) {
+            $panelEditor.empty().append(_textHtml);
+        }
+    },
+    imgEditOpenFn : function ($panel, $li) {
+        var arr= this.showPanelFn();
+        arr._panel.find(this.initialOptions._dot + this.initialOptions.imgPanel).removeClass(this.initialOptions.hideClass);
     },
     render: function() {
         return (
@@ -47,10 +78,10 @@ var EditBar = React.createClass({
                     <div className="float-btn-group">
                         <button className="btn-float btn-triger pink"><i className="icon-bars"></i></button>
                         <div className="btn-list">
-                            <a href="javascript:void(0);" className="btn-float blue" onClick={this.panelOpenHandler}><i className="fa fa-align-justify"></i></a>
-                            <a href="javascript:void(0);" className="btn-float blue"><i className="fa fa-cloud-download"> </i></a>
-                            <a href="javascript:void(0);" className="btn-float blue"><i className="fa fa-paperclip">   </i></a>
-                            <a href="javascript:void(0);" className="btn-float blue"><i className="fa fa-floppy-o"> </i></a>
+                            <a href="javascript:void(0);" className="btn-float blue" onClick={this.txtEditOpenFn}><i className="fa fa-pencil"></i></a>
+                            <a href="javascript:void(0);" className="btn-float blue" onClick={this.imgEditOpenFn}><i className="fa fa-file-image-o"> </i></a>
+                            <a href="javascript:void(0);" className="btn-float blue" data-edit="act"><i className="fa fa-cloud-upload">   </i></a>
+                            <a href="javascript:void(0);" className="btn-float blue" data-edit="del"><i className="fa fa-trash-o fa-fw"> </i></a>
                         </div>
                     </div>
                 </div>
@@ -58,6 +89,5 @@ var EditBar = React.createClass({
         );
     }
 });
-
 module.exports = EditBar;
 
--- modules/panel/EditorPanel.jsx
+++ modules/panel/EditorPanel.jsx
@@ -1,68 +1,103 @@
-require("../rtfeditor/mytexteditor");
+/*require("../rtfeditor/mytexteditor");*/
 require("./panel.css");
 var React = require('react');
 var $ = require("jquery");
 
 var EditPanel = React.createClass({
     initialOptions : {
-        panelId: "#j-edit-panel",
-        barId : "#j-edit-bar",
-        gridClass : ".j-gridster",
-        panelText: ".edit-panel-text",
-        gridBlockClass: ".j-grid-block",
-        editorClass: ".wysiwyg-editor",
+        _dot : ".",
+        _id: "#",
+        panelId: "j-edit-panel",
+        barId : "j-edit-bar",
+        txtPanel: "j-panel-text",
+        imgPanel: "j-panel-img",
+        gridClass : "j-gridster",
+        panelText: "edit-panel-text",
+        gridBlockClass: "j-grid-block",
         hideClass: "hide",
-        dataPanelIndex: "data-panel-index"
+        dataPanelIndex: "data-panel-index",
+        myGridContainer: "j-grid-container"
 
     },
-
     statics: {
         init: function() {
             $(".j-grid-block").on("mouseover", function () {
                 var $offset = $(this).offset(),
                     _top = $offset.top,
-                    _left = $offset.left;
-                console.log($offset);
-                $("#j-edit-bar").css({"top":_top + 5,"left":_left + 10}).removeClass("hide").attr("data-bar-index",$(this).index());
+                    _W = $(this).innerWidth(),
+                    _left = $offset.left + _W -260;
+                $("#j-edit-bar").css({"top":_top +5,"left":_left }).fadeIn().attr("data-bar-index",$(this).index());
             });
 
-            $(".edit-panel-title").dragDiv("#j-edit-panel");
+            $(".panel-drag").dragDiv("#j-edit-panel");
 
             $('.btn-triger').click(function () {
                 $(this).closest('.float-btn-group').toggleClass('open');
             });
         }
     },
+    txtPanelFn: function () {
+        var $textElem = $(this.initialOptions._dot + this.initialOptions.panelText),
+            $createdHtml= $textElem.html(),
+            $blank = $textElem.find(".placeholder");
+        var _indx =  $(this.initialOptions._id + this.initialOptions.panelId).attr(this.initialOptions.dataPanelIndex);
+        var $gridLi = $(this.initialOptions._dot + this.initialOptions.gridBlockClass).eq(_indx),
+            $tempLi = $gridLi.find(this.initialOptions._dot + this.initialOptions.myGridContainer);
 
-    closePanelHandler: function () {
-        var $createdHtml = $(this.initialOptions.panelText).find(this.initialOptions.editorClass);
-        var _indx =  $(this.initialOptions.panelId).attr(this.initialOptions.dataPanelIndex);
-        var $gridLi = $(this.initialOptions.gridBlockClass).eq(_indx),
-            $tempLi = $gridLi.find(this.initialOptions.editorClass);
-        if ($tempLi.length) {
-            $tempLi.remove();
-            $gridLi.append($createdHtml);
+        if ($blank.length < 1) {
+            $tempLi.html($createdHtml);
         }else {
-            $gridLi.append($createdHtml);
+            $tempLi.empty();
         }
-        $(this.initialOptions.panelId).addClass(this.initialOptions.hideClass);
+        $(this.initialOptions._dot + this.initialOptions.txtPanel).addClass(this.initialOptions.hideClass);
+    },
+    imgPanelFn:function(){
+        $(this.initialOptions._dot + this.initialOptions.imgPanel).addClass(this.initialOptions.hideClass);
+    },
+    closePanelHandler: function () {
+        var $panel =$(this.initialOptions._id + this.initialOptions.panelId);
+        this.txtPanelFn();
+        this.imgPanelFn();
+        $panel.fadeOut();
     },
     render: function () {
-        var panelId = this.initialOptions.panelId;
+        var _nameEditor = {
+            txt: "Text Settings",
+            img: "Image Settings"
+        };
+        var _panelId = this.initialOptions.panelId;
         return (
-            <div  id="j-edit-panel" className="edit-panel panel-drag hide">
-                <div className="" >
-                    <div className="edit-panel-title">
-                        <span className="">Editor</span>
-                        <span className="edit-panel-close" onClick={this.closePanelHandler}>CLOSE</span>
+
+            <div  id={_panelId} className="edit-panel panel-drag">
+                <div className="j-panel-text hide">
+                    <div className="edit-panel-title panel-drag">
+                        <span className="bold">{_nameEditor.txt}</span>
+                            <span  className="edit-panel-close" onClick={this.closePanelHandler}>
+                            <i className="fa fa-times-circle"> </i>
+                        </span>
                     </div>
                     <div className="edit-panel-text"></div>
                 </div>
+                <div className="j-panel-img panel-img-pop panel-drag hide">
+                    <div className="pop_title">{_nameEditor.img} <a href="javascript:void(0);" onClick={this.closePanelHandler} >x</a></div>
+                    <div className="pop_main">
+                        <div className="main_editbg"></div>
+                        <p><a href="#">Change</a></p>
+                        <p><a href="#">Edit</a></p>
+                    </div>
+                    <div className="pop_content">
+                        <p>How's this image resized?</p>
+                        <div className="content_list"><a href="javascript:;"><img src="images/01.png" width="29" height="20" /></a><span>Autocenter</span></div>
+                        <div className="content_list"><a href="javascript:;"><img src="images/02.png" width="29" height="30" /></a><span>center</span></div>
+                        <div className="content_list"><a href="javascript:;"><img src="images/03.png" width="29" height="30" /></a><span>Stretch</span></div>
+                        <div className="content_list"><a href="javascript:;"><img src="images/04.png" width="29" height="20" /></a><span>Fit</span></div>
+                    </div>
+                    <div className="pop_footer">Image Text</div>
+                </div>
             </div>
-        )
-    }
-});
+        ) }
 
+});
 
 module.exports = EditPanel;
 
