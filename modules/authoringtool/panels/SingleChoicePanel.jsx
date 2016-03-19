b5780ff370d23f266fcbe484fe53498acac90e73
qcgm1978 <zhanghl@strongunion.com.cn>
2016-03-19 14:18:28 +0800

    duplicated id bug fix

--- modules/authoringtool/panels/SingleChoicePanel.jsx
+++ modules/authoringtool/panels/SingleChoicePanel.jsx
@@ -1,64 +1,51 @@
 var React = require('react');
-
 var RightPanelMixin = require("./RightPanelMixin.jsx");
 var LinkedStateMixin = require('react-addons-linked-state-mixin');
 var AuthoringInfo = require("../AuthoringInfo");
-
 var postal = require("postal");
-
 var SingleChoicePanel = React.createClass({
-
-    NAME : '',
-
+    NAME: '',
     mixins: [RightPanelMixin, LinkedStateMixin], // Use the mixin
-
-    statics : {
-      renderHTML: function(json) {
-        /*use handdlebar template later */
-
-        var html = '<div class="activity_single_choice generated questions_list">';
-        html += '<div class="list">';
-        html += '<ul class="check_list right_side">';
-        html += '<p class="question_tit pb20"><span></span>' + json.text + '</p>';
-        json.choices.map(function(choice, index) {
-            html +=  '<li><i></i>';
-            html +=  '<span class="letters">' + index + '</span>';
-            html +=  '<a href="javascript:;" class="choose">';
-            html +=  '<input type="radio" name="name11"/></a>'
-            html += '<span>' + choice.text + '</span>';
-            html += '</li>';
-          });
-          html += '</ul>';
-          html += "</div>";
-          html += '</div>';
-          return html;
-      }
+    statics: {
+        renderHTML: function (json) {
+            /*use handdlebar template later */
+            var html = '<div class="activity_single_choice generated questions_list">';
+            html += '<div class="list">';
+            html += '<ul class="check_list right_side">';
+            html += '<p class="question_tit pb20"><span></span>' + json.text + '</p>';
+            json.choices.map(function (choice, index) {
+                html += '<li><i></i>';
+                html += '<span class="letters">' + index + '</span>';
+                html += '<a href="javascript:;" class="choose">';
+                html += '<input type="radio" name="name11"/></a>'
+                html += '<span>' + choice.text + '</span>';
+                html += '</li>';
+            });
+            html += '</ul>';
+            html += "</div>";
+            html += '</div>';
+            return html;
+        }
     },
     getInitialState: function () {
         return {
             width: 640,
-            answer:  this.props.json
+            answer: this.props.json
         }
     },
-
-    getName: function() {
+    getName: function () {
         return "Single Choice";
     },
-
     componentDidMount: function () {
-
     },
-
-    deleteChoice: function(index) {
-        this.state.answer.choices.splice(index,1);
+    deleteChoice: function (index) {
+        this.state.answer.choices.splice(index, 1);
         this.setState({
             answer: this.state.answer
         });
-
         this.publishChange(this.state.answer);
     },
-
-    publishChange: function(json) {
+    publishChange: function (json) {
         postal.publish({
             channel: "block",
             topic: "modified",
@@ -70,22 +57,19 @@ var SingleChoicePanel = React.createClass({
         });
         AuthoringInfo.data.widgetJSON[this.props.blockId] = json;
     },
-
-    componentWillReceiveProps: function(nextProps) {
+    componentWillReceiveProps: function (nextProps) {
         this.setState({
-            answer:  nextProps.json
+            answer: nextProps.json
         });
     },
-
-    questionChanged: function(event) {
-        this.state.answer.text  = event.target.value;
+    questionChanged: function (event) {
+        this.state.answer.text = event.target.value;
         this.setState({
             answer: this.state.answer
         });
         this.publishChange(this.state.answer);
     },
-
-    choiceChanged: function(event) {
+    choiceChanged: function (event) {
         var target = $(event.target);
         this.state.answer.choices[parseInt(target.data("index"))].text = event.target.value;
         this.setState({
@@ -93,41 +77,39 @@ var SingleChoicePanel = React.createClass({
         });
         this.publishChange(this.state.answer);
     },
-
-    renderHTML: function() {
+    renderHTML: function () {
         var mm = (<ul className="check_list">
             <li className="">
                 <i></i>
                 <span className="letters">A</span>
                 <a href="javascript:;" className="choose">
                     <input type="radio" name="name11"/>
-                    </a>
-                    <span>monkeys are not usually right-handed.</span>
+                </a>
+                <span>monkeys are not usually right-handed.</span>
             </li>
-            </ul>);
+        </ul>);
         return mm;
     },
-    getUniqueId:()=>{
-      return Math.random()
+    getUniqueId: ()=> {
+        return Math.random()
     },
-    renderEditor: function() {
+    renderEditor: function () {
         var panel = this;
-
-        var choices = this.state.answer.choices.map(function(choice, index) {
-            return <div className="form-group">
-                   <input type="radio" name="sc"/> <input type="text" key={panel.getUniqueId} defaultValue={choice.text} data-index={index} onChange={panel.choiceChanged}/> <a className="delete" onClick={panel.deleteChoice.bind(panel, index)}>Del</a>
-                </div>
+        var choices = this.state.answer.choices.map(function (choice, index) {
+            return <div key={panel.getUniqueId()} className="form-group">
+                <input type="radio" name="sc"/> <input type="text"  defaultValue={choice.text}
+                                                       data-index={index} onChange={panel.choiceChanged}/> <a
+                className="delete" onClick={panel.deleteChoice.bind(panel, index)}>Del</a>
+            </div>
         });
-
         return <div>
             <div className="form-group">
                 <label >Question</label>
-                <textarea className="form-control" value={this.props.json.text} onChange={this.questionChanged}></textarea>
+                <textarea className="form-control" value={this.props.json.text}
+                          onChange={this.questionChanged}></textarea>
             </div>
             {choices}
         </div>
     }
-
 });
-
 module.exports = SingleChoicePanel;
